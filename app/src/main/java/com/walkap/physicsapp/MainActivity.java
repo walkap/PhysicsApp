4c9e7aaa8af243198cd003a61cc8cb663888d892
Roberto Capannelli <roberto.capannelli@gmail.com>
2016-12-31 12:04:46 +0100

    null

--- app/src/main/java/com/walkap/physicsapp/MainActivity.java
+++ app/src/main/java/com/walkap/physicsapp/MainActivity.java
@@ -6,13 +6,11 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
     public void startGame(View view) {
         Intent intent = new Intent(this, WorldActivity.class);
         startActivity(intent);
