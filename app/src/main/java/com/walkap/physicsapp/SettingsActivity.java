82bfc0d6f5d5fd00e342355d3a18603b30b8ac05
Roberto Capannelli <roberto.capannelli@gmail.com>
2017-01-02 20:18:48 +0100

    some changes for checkbox gravity

--- app/src/main/java/com/walkap/physicsapp/SettingsActivity.java
+++ app/src/main/java/com/walkap/physicsapp/SettingsActivity.java
@@ -2,17 +2,15 @@ package com.walkap.physicsapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.RadioButton;
 
 import org.jbox2d.common.Vec2;
-import org.jbox2d.dynamics.World;
 
-import static com.walkap.physicsapp.WorldView.world;
 
 
 public class SettingsActivity extends AppCompatActivity {
@@ -30,30 +28,22 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
         et=(EditText) findViewById(R.id.editText);
         et.setVisibility(View.VISIBLE);
-
+        cb = (CheckBox) findViewById(R.id.cbGravity);
 
     }
 
-
-
-
-
-    public void onCheckButtonClicked(View view) {
-
-        boolean checked = ((CheckBox) view).isChecked();
-
-        if (checked) {
-            et.setVisibility(View.GONE);
-            world.setGravityDefault(true);}
-        else {
+    private void setGravityManually(){
+        if(cb.isChecked()){
+            et.setVisibility(View.INVISIBLE);
+            world.setGravityDefault(true);
+        }else{
             et.setVisibility(View.VISIBLE);
             world.setGravityDefault(false);
             y = Float.valueOf(String.valueOf(et.getText()));
             gravity = new Vec2(x,y);
-            world.setGravity(gravity); }
-
+            world.setGravity(gravity);
+        }
     }
+
+
 }
-
-
-
