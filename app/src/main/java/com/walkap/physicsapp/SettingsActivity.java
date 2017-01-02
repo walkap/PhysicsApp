373e51dec65f57a7f7486efcc81c9b1aedee1eff
luca92 <l.atturo@hotmail.it>
2017-01-02 19:00:51 +0100

    setting activity (ERRORI)

--- app/src/main/java/com/walkap/physicsapp/SettingsActivity.java
+++ app/src/main/java/com/walkap/physicsapp/SettingsActivity.java
@@ -2,22 +2,26 @@ package com.walkap.physicsapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.World;
 
+import static com.walkap.physicsapp.WorldView.world;
 
-/**
- * Created by luca92 on 02/01/17.
- */
 
 public class SettingsActivity extends AppCompatActivity {
     RadioButton rad;
     CheckBox cb;
     EditText et;
+    WorldView world;
+    Vec2 gravity;
+    float x = 0.0f , y;
 
 
     @Override
@@ -26,17 +30,28 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
         et=(EditText) findViewById(R.id.editText);
         et.setVisibility(View.VISIBLE);
+
+
     }
+
+
+
 
 
     public void onCheckButtonClicked(View view) {
 
         boolean checked = ((CheckBox) view).isChecked();
 
-        if(checked)
+        if (checked) {
             et.setVisibility(View.GONE);
-        else
+            world.setGravityDefault(true);}
+        else {
             et.setVisibility(View.VISIBLE);
+            world.setGravityDefault(false);
+            y = Float.valueOf(String.valueOf(et.getText()));
+            gravity = new Vec2(x,y);
+            world.setGravity(gravity); }
+
     }
 }
 
