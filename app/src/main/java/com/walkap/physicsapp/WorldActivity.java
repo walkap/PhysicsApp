5943daa7b4c5e1eea7f2800e7cd7e7bf2d6735a5
Roberto Capannelli <roberto.capannelli@gmail.com>
2017-01-03 11:48:20 +0100

    listener for reset button

--- app/src/main/java/com/walkap/physicsapp/WorldActivity.java
+++ app/src/main/java/com/walkap/physicsapp/WorldActivity.java
@@ -1,16 +1,38 @@
 package com.walkap.physicsapp;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
-public class WorldActivity extends AppCompatActivity {
+import static com.walkap.physicsapp.WorldView.world;
+
+public class WorldActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button resetButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_world);
+
+        resetButton = (Button) findViewById(R.id.resetButton);
+
+        resetButton.setOnClickListener(this);
+    }
+
+    public void onClick(View resetButton){
+        try
+        {
+            world.resetWorld();
+        }
+        catch (Exception e)
+        {
+            Log.e("WorldActivity","error in Reset");
+        }
     }
 
     @Override
