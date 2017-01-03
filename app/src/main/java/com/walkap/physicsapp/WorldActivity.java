b09633d7faa6e9c7ec9a3e6cba97039c52aa8ebf
Roberto Capannelli <roberto.capannelli@gmail.com>
2017-01-03 12:03:30 +0100

    -

--- app/src/main/java/com/walkap/physicsapp/WorldActivity.java
+++ app/src/main/java/com/walkap/physicsapp/WorldActivity.java
@@ -1,6 +1,5 @@
 package com.walkap.physicsapp;
 
-import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,39 +12,30 @@ import static com.walkap.physicsapp.WorldView.world;
 public class WorldActivity extends AppCompatActivity implements View.OnClickListener{
 
     Button resetButton;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_world);
 
         resetButton = (Button) findViewById(R.id.resetButton);
-
         resetButton.setOnClickListener(this);
     }
 
     public void onClick(View resetButton){
-        try
-        {
+        try{
             world.resetWorld();
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             Log.e("WorldActivity","error in Reset");
         }
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        try
-        {
+        try {
             WorldView.TouchEvent(event);
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             Log.e("WorldActivity","error in TouchEvent");
         }
-
         return true;
     }
 }
